6a04476d2eb5692784cec4212c91d544aa6a37ca
Coasting-Now <costakokis@hotmail.com>
2024-12-11 18:50:12 +1100

    update_11.12

--- Task_8.1__sf/guess-number-task/game_v3.py
+++ Task_8.1__sf/guess-number-task/game_v3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def game_core_v3(number: int = 1) -> int:
     """
     Args:
@@ -31,3 +33,29 @@ def game_core_v3(number: int = 1) -> int:
     # Ваш код заканчивается здесь
 
     return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(game_core_v3(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(game_core_v3)
